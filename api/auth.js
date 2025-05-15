c816688012fc11056845d341e6c722d6b776a15b
Rozenbilds <gints.roz@inbox.lv>
2025-05-15 16:31:52 +0300

    Update auth.js

--- api/auth.js
+++ api/auth.js
@@ -4,5 +4,5 @@ import { createOAuthHandler } from "@decapcms/netlify-cms-oauth";
 export default createOAuthHandler({
   origin: "https://gintsroz.pages.dev", // ← tava Decap CMS lapa
   clientId: "Ov23liWzlOWBvdlAqohZ",    // no GitHub OAuth lietotnes
-  clientSecret: "339d9c72678fffefa1fe3aef437efd6a4eaf5efa", // slepenais atslēgas kods
+  clientSecret: "d72afc0dc12318fdc482a2f5fc42eb651d797cda", // slepenais atslēgas kods
 });
