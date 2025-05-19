b6f03b6b80c0626bc4c57f3c27762bccfc876a04
Rozenbilds <gints.roz@inbox.lv>
2025-05-19 22:28:58 +0300

    Delete api directory

--- api/auth.js
+++
@@ -1,8 +0,0 @@
-// api/auth.js
-import { createOAuthHandler } from "@decapcms/netlify-cms-oauth";
-
-export default createOAuthHandler({
-  origin: "https://gintsroz.pages.dev", // ← tava Decap CMS lapa
-  clientId: "Ov23liWzlOWBvdlAqohZ",    // no GitHub OAuth lietotnes
-  clientSecret: "3e8fc4efbb53018298fe5c5aafeb77462ec88f25", // slepenais atslēgas kods
-});
